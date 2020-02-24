89c6cec4aca8417ec000a55ed3e3c70f181f34db
Erwan Normand <normand.erwan@protonmail.com>
2020-02-24 23:48:23 +0100

    :sparkles: Add Material struct

---
+++ Assets/Scripts/Material.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.Mathematics;
+
+namespace NormandErwan.TinyRayTracer
+{
+    /// <summary>
+    /// Represents shader data for <see cref="UnityEngine.Material"/>.
+    /// </summary>
+    public struct Material
+    {
+        /// <summary>
+        /// Creates a new <see cref="Material"/> from a <see cref="UnityEngine.Material"/>.
+        /// </summary>
+        /// <param name="material">The source <see cref="UnityEngine.Material"/> to copy.</param>
+        public Material(UnityEngine.Material material)
+        {
+            Color = (Vector4)material.color;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="UnityEngine.Material.color"/>.
+        /// </summary>
+        public float4 Color { get; }
+    }
+}
---
+++ Assets/Scripts/Material.hlsl
@@ -0,0 +1,11 @@
+#ifndef __MATERIAL__
+#define __MATERIAL__
+
+// Represents a material.
+struct Material
+{
+    // The color of this material.
+    float4 Color;
+};
+
+#endif //__MATERIAL__
