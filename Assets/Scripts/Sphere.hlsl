9460c7114a5e6cc45c26002681a53da7d5739288
Erwan Normand <normand.erwan@protonmail.com>
2020-02-25 23:25:15 +0100

    :sparkles: Add Intersect(Ray, Sphere)

--- Assets/Scripts/Sphere.hlsl
+++ Assets/Scripts/Sphere.hlsl
@@ -2,6 +2,7 @@
 #define __SPHERE__
 
 #include "Assets/Scripts/Material.hlsl"
+#include "Assets/Scripts/Ray.hlsl"
 
 // Represents a 3D sphere.
 struct Sphere
@@ -15,5 +16,20 @@ struct Sphere
     // The radius of this sphere.
     float Radius;
 };
+
+bool Intersect(in Ray ray, in Sphere sphere, out float hitDistance, out float3 hitPoint)
+{
+    bool isOnRay = Project(ray, sphere.Center, hitDistance);
+    if (!isOnRay)
+    {
+        return false;
+    }
+
+    hitPoint = GetPoint(ray, hitDistance);
+
+    float centerHitDistance = distance(sphere.Center, hitPoint);
+    bool intersect = centerHitDistance <= sphere.Radius;
+    return intersect;
+}
 
 #endif //__SPHERE__
