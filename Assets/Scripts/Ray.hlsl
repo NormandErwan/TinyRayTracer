2f78e75ef96c08508f442094adbd4193e0d29e39
Erwan Normand <normand.erwan@protonmail.com>
2020-02-25 23:24:58 +0100

    :sparkles: Add Ray.hlsl

---
+++ Assets/Scripts/Ray.hlsl
@@ -0,0 +1,29 @@
+﻿#ifndef __RAY__
+#define __RAY__
+
+// Represents a ray.
+struct Ray
+{
+    // The origin of this ray.
+    float3 Origin;
+
+    // The direction of this ray.
+    float3 Direction;
+};
+
+// Project a position into a Ray.
+bool Project(in Ray ray, in float3 position, out float distance)
+{
+    distance = dot(ray.Direction, position);
+
+    bool isOnRay = distance >= 0;
+    return isOnRay;
+}
+
+// Get a position along a Ray.
+float3 GetPoint(in Ray ray, float distance)
+{
+    return ray.Origin + distance * ray.Direction;
+}
+
+#endif //__RAY__
